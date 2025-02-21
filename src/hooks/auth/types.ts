70214037982d2169e8f8da615d800780681675bb
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2025-02-21 16:42:22 -0300

    feat - creating auth hook

---
+++ src/hooks/auth/types.ts
@@ -0,0 +1,5 @@
+export type TSignUpCredentials = {
+
+  email: string;
+  password: string;
+};
---
+++ src/hooks/auth/useAuth.ts
@@ -0,0 +1,40 @@
+import { supabase } from "@/src/lib/supabase";
+import { useState } from "react";
+import { TSignUpCredentials } from "./types";
+import { Alert } from "react-native";
+import { router } from "expo-router";
+
+export const useAuth = () => {
+  const [isLoadingAuth, setIsLoadingAuth] = useState(false);
+
+  const handleSignUp = async (credentials: TSignUpCredentials) => {
+    const { email, password } = credentials;
+
+    try {
+      setIsLoadingAuth(true);
+
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+
+      });
+
+      if (error) {
+        return Alert.alert("Erro ao cadastrar", error.message);
+      }
+
+      Alert.alert("Sucesso", "Cadastro realizado com sucesso");
+
+      router.replace("/");
+    } catch (error) {
+      console.error("Erro ao cadastrar", error);
+    } finally {
+      setIsLoadingAuth(false);
+    }
+  }
+
+  return {
+    isLoadingAuth,
+    handleSignUp,
+  };
+};
